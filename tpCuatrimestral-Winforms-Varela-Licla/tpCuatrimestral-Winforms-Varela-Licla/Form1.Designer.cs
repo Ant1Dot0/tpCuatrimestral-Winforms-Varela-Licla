c81c40aa04b7a081cb5c4d1543aa92b70ff0e4f0
Ant1Dot0 <joselicla021@gmail.com>
2022-04-30 00:57:12 -0300

    nuevos cambios

--- tpCuatrimestral-Winforms-Varela-Licla/tpCuatrimestral-Winforms-Varela-Licla/Form1.Designer.cs
+++ tpCuatrimestral-Winforms-Varela-Licla/tpCuatrimestral-Winforms-Varela-Licla/Form1.Designer.cs
@@ -33,7 +33,7 @@ namespace tpCuatrimestral_Winforms_Varela_Licla
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -76,12 +76,12 @@ namespace tpCuatrimestral_Winforms_Varela_Licla
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(607, 323);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "edu";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button5.Location = new System.Drawing.Point(607, 323);
+            this.button5.Name = "button2";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "edu";
+            this.button5.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -92,7 +92,7 @@ namespace tpCuatrimestral_Winforms_Varela_Licla
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -106,7 +106,7 @@ namespace tpCuatrimestral_Winforms_Varela_Licla
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button5;
     }
 }
 
